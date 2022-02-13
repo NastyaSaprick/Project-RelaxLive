4192b09ee7d277bb692c6155da8d7af0c74cd76b
NastyaSaprick <sapronovaas2002@gmail.com>
2022-02-13 15:10:38 +0300

    19. Блок ЧаВо (Часто задаваемые вопросы) реализовать в виде аккордеона

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import sliders  from "./modules/sliders";
 import modal  from "./modules/modal";
 import menu  from "./modules/menu";
+import accordion  from "./modules/accordion";
 
 
 sliders()
 modal()
 menu()
+accordion()
 //npm run watch
---
+++ src/modules/accordion.js
@@ -0,0 +1,19 @@
+const accordion = () => {
+//19. Аккардион 
+  const accordeon = document.querySelector('.accordion')
+  const titles = document.querySelectorAll('.title_block')
+
+  accordeon.addEventListener('click', (e) => {
+    const target = e.target
+
+    titles.forEach(accord => {
+      if (accord === target && !accord.classList.contains('msg-active')) {
+        accord.classList.add('msg-active')
+      } else {
+        accord.classList.remove('msg-active')
+      }
+    })
+  })
+}
+
+export default accordion
