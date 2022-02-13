2382c63699e823e0bff5126fccecbe5c88be394f
NastyaSaprick <sapronovaas2002@gmail.com>
2022-02-13 18:02:53 +0300

    добавила закрытие меню через элемент списка

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -4,7 +4,8 @@ const menu = () => {
   const menuBlock = document.querySelector('.popup-dialog-menu')//модаьное окно
   const btnOpen = document.querySelector('.menu__icon')//кнопка меню которая открывает модальное окно
   const menuBtnClsoe = document.querySelector('.close-menu')//кнопка которая закрывает модальное окно
-
+  const menuBtnClsoeLink = menuBlock.querySelectorAll('.menu-link')//кнопка которая закрывает модальное окно через элемент списка
+console.log(menuBtnClsoeLink);
   //адаптация 
   if (window.matchMedia("(min-width: 576px)").matches) {
       btnOpen.addEventListener("click", () => {
@@ -19,18 +20,36 @@ const menu = () => {
               menuBlock.style.transform = "translate3d(845px,0,0)"
           }
       });
-    } else {
+      //закрывает меню через элемент списка
+      menuBtnClsoeLink.forEach(btn => {
+        btn.addEventListener('click', () => {
+          if (menu.style.visibility = "visible") {
+            menu.style.visibility = "hidden"
+            menuBlock.style.transform = "translate3d(845px,0,0)"
+        }
+        })
+      });
+    } else { //открывает меню
       btnOpen.addEventListener("click", () => {
           if (menu.style.visibility = "hidden") {
               menu.style.visibility = "visible"
               menuBlock.style.transform = "translate3d(0px,0,0)"
           }
-      });
+      });//закрывает меню
       menuBtnClsoe.addEventListener("click", () => {
           if (menu.style.visibility = "visible") {
               menu.style.visibility = "hidden"
               menuBlock.style.transform = "translate3d(0px, -150vh,0)"
           }
+      });
+      //закрывает меню через элемент списка
+      menuBtnClsoeLink.forEach(btn => {
+        btn.addEventListener('click', () => {
+          if (menu.style.visibility = "visible") {
+            menu.style.visibility = "hidden"
+            menuBlock.style.transform = "translate3d(845px,0,0)"
+        }
+        })
       });
     }
 
@@ -49,5 +68,6 @@ const menu = () => {
   closeBtnMenu.addEventListener('click', () => {
     modalMenu.style.display = 'none'
   })
+
 }
 export default menu
